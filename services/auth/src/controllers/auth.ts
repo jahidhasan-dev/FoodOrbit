11dc9982d0d533e53bd8d55219f52cb2a39102ae
exact-coder <hasanakash799@gmail.com>
2026-05-02 22:21:57 +0600

    login  functionality added

--- services/auth/src/controllers/auth.ts
+++ services/auth/src/controllers/auth.ts
@@ -1,6 +1,26 @@
-import { Request, Response } from "express";
+import User from "../model/User.js";
+import jwt from "jsonwebtoken";
+import TryCatch from "../middlewares/trycatch.js";
 
 
-export const loginUser = async(req: Request, res: Response) => {
-    res.json(req.body);
-}
+export const loginUser = TryCatch(async(req, res) => {
+    const { email, name, picture } = req.body;
+
+    let user = await User.findOne({email});
+
+    if(!user) {
+        user = await User.create({
+            name, email, image: picture,
+        });
+    }
+
+    const token = jwt.sign({ user }, process.env.JWT_SEC as string, {
+        expiresIn: "15d",
+    });
+    res.status(200).json({
+        message: "Logged In Successfully",
+        token,
+        user,
+    });
+});
+
--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -20,4 +20,4 @@ app.listen(PORT, () => {
 });
 
 
-// 1:1min
+// 1:14min
